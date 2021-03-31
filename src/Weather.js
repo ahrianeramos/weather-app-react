51cabd4766ef039eed604ade6384ff5a1250b7c4
Ahriane Ramos <76123138+misspx12@users.noreply.github.com>
2021-03-31 02:32:19 -0700

    Added Search Engine; Updated CSS

--- src/Weather.js
+++ src/Weather.js
@@ -6,7 +6,26 @@ import axios from "axios";
 export default function Weather(props) {
   return (
     <div className="Weather">
-      <div className="Location-header">
+      <form className="row">
+        <div className="col-auto">
+          <input
+            type="search"
+            placeholder="Enter a city"
+            className="form-control"
+            id="search-field"
+          />
+        </div>
+        <div className="col-auto">
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-primary mb-1"
+            id="search-btn"
+            autocomplete="off" />
+        </div>
+      </form>
+
+      <div className="location-header">
         <div className="col-auto" id="location">San Francisco</div>
         <div className="col-6" id="current-date">Wednesday 01:06</div>
         <div className="col-6" id="description">Clear Sky</div>
@@ -14,7 +33,11 @@ export default function Weather(props) {
 
       <div className="row">
         <div className="col-2" id="icon-position">
-          <img src="https://openweathermap.org/img/wn/01n@2x.png" id-="temp-icon" width="100" alt="temperature icon" />
+          <img 
+            src="https://openweathermap.org/img/wn/01n@2x.png" 
+            id="temp-icon" 
+            width="100" 
+            alt="temperature icon" />
         </div>
         <div className="col-auto" id="temperature">13</div>
         <div className="col-auto" id="temp-units">°C | °F</div>
