6d974521c40c128553d1f4a832af83d0ea3bc98e
Ahriane Ramos <76123138+misspx12@users.noreply.github.com>
2021-03-31 00:00:59 -0700

    Added link to GitHub repo

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import './App.css';
 import Weather from "./Weather";
 
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Weather city="San Francisco" />
       </header>
+
+      <footer>
+        <a href="https://github.com/misspx12/weather-app-react">Open-source code</a>
+        {" "}by Ahriane 💻
+      </footer>
     </div>
   );
 }
 
-export default App;
