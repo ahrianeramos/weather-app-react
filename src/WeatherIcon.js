e3f4d7279068610eadf1dad42158614ba040ab8e
Ahriane Ramos <76123138+misspx12@users.noreply.github.com>
2021-05-04 14:59:32 -0700

    fixed styling

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="#000000"
+      color="#3B978D"
       size={props.size}
       animate={true}
       />
